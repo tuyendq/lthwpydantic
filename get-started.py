7f08bbd001e69ce8e7cb7484af00226077132c6a
Tuyen Dang <tuyendq@gmail.com>
2025-03-25 23:43:07 +0700

    set Default value

--- get-started.py
+++ get-started.py
@@ -3,8 +3,18 @@ from pydantic import BaseModel
 # Define UserProfile model
 class UserProfile(BaseModel):
     name: str
-    age: int
+    age: int = 18  # Default value
     email: str
+    is_active: bool = True  # Default value
 
 user = UserProfile(name="Tuyen ", age=18, email="tuyen@example.com")
 print(user)
+
+# Working with model attributes
+print(user.name)
+print(user.age)
+print(user.email)
+
+
+user2 = UserProfile(name="Tuyen ", email="tuyen@example.com")
+print(user2)
